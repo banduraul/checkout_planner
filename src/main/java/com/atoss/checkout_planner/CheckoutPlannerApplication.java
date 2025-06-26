93f721bfdf8d0193cd41d774ccab0481688a6bf5
Raul-Valentin Bandu <banduraul0@gmail.com>
2025-06-26 18:49:30 +0300

    Initial commit

---
+++ src/main/java/com/atoss/checkout_planner/CheckoutPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.atoss.checkout_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atoss/checkout_planner/CheckoutPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atoss.checkout_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
